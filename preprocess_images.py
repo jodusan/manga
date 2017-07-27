b2828a856147d3fb9a61a43e6f47134e8be45c4f
Dusan <dusanix@gmail.com>
2017-07-27 10:14:25 +0200

    Add segmentation and desegmentation

--- preprocess_images.py
+++ preprocess_images.py
@@ -1,6 +1,8 @@
+from pprint import pprint
+
 import numpy as np
 from scipy import misc
-
+import time
 
 #
 # height = 950
@@ -67,21 +69,66 @@ from scipy import misc
 # plt.hist(coorelation_coefs)
 # plt.show()
 # print(coorelation_coefs)
+from dataset import load_all_images
 
-def generate_bw_image(im, axis=2):
-    # Alternate version:
-    # r = im[:, :, 0] > 50
-    # g = im[:, :, 1] > 30
-    # b = im[:, :, 2] > 50
-    # n = (r * g * b) > 0
-    # nn = (r * g * b) == 0
-    # im[n] = 255
-    # im[nn] = 0
-    # return im
 
-    return np.mean(im, axis=axis) > 60
+def generate_bw_image(img, axis=2):
+    return np.mean(img, axis=axis) > 60
+
+
+def bin_colors(img, number_of_bins=2):
+    bin_size = 256 // number_of_bins
+    res = np.zeros(img.shape, dtype=np.uint8)
+    for i in range(number_of_bins):
+        mask = np.logical_and((img >= (bin_size * i)), (img < (bin_size * (i + 1))))
+        res += (mask * (bin_size * i + bin_size // 2)).astype(np.uint8)
+    return res
+
+
+def image_segmentation(img, number_of_bins):
+    res = np.zeros((img.shape[0], img.shape[1], number_of_bins ** 3), dtype=np.bool)
+    bin_size = 256 // number_of_bins
+    color_indexed_img = img // bin_size
+    for i in range(number_of_bins):
+        for j in range(number_of_bins):
+            for k in range(number_of_bins):
+                r = color_indexed_img[:, :, 0] == i
+                g = color_indexed_img[:, :, 1] == j
+                b = color_indexed_img[:, :, 2] == k
+                mask = r * b * g
+                res[:, :, k + j * number_of_bins + i * number_of_bins ** 2] = mask
+    return res
+
+
+def image_desegmentation(segmented_image, number_of_bins):
+    result = np.zeros((segmented_image.shape[0], segmented_image.shape[1], 3), dtype=np.uint8)
+    bin_size = 256 // number_of_bins
+    for i in range(segmented_image.shape[2]):
+        b = i % number_of_bins
+        g = i // number_of_bins % number_of_bins
+        r = i // (number_of_bins ** 2) % number_of_bins
+        pixel_value = bin_size * np.array([r, g, b], dtype=np.uint8) + bin_size // 2
+        result[segmented_image[:, :, i]] = pixel_value
+
+    return result
 
 
 if __name__ == "__main__":
-    im = misc.imread("train_dataset/montage_color.jpg")
-    generate_bw_image(im)
+    bins = 2
+    depth = bins ** 3
+    images = load_all_images('train_dataset/color', False, resize_x=512, resize_y=768)
+    print("loaded dataset")
+    start_time = time.time()
+    y = np.empty((len(images), images[0].shape[0], images[0].shape[1], depth), dtype=np.uint8)
+    for i in range(len(images)):
+        im = bin_colors(images[i], 2)
+        y[i] = image_segmentation(im, 2)
+    print(time.time() - start_time)
+    print("finished")
+    start_time = time.time()
+    np.save("train_dataset/color_seg", y)
+    print(time.time() - start_time)
+    del y
+    start_time = time.time()
+    y = np.load("train_dataset/color_seg.npy")
+    print(time.time() - start_time)

53e1b5072d14650492ef9c6a925b757e58c5ac1c
Nikola Jovicic <jovke1994@gmail.com>
2017-11-17 12:10:07 +0100

    Predict all files in folder

--- predict.py
+++ predict.py
@@ -1,11 +1,12 @@
 import sys
 
-from matplotlib.pyplot import imshow, show
+from matplotlib.pyplot import imshow, show, imsave
 from scipy import misc
 
 from preprocess_images import generate_adaptive_bw_image
 from unet_online import get_unet
 import numpy as np
+import os
 
 
 def main():
@@ -18,15 +19,23 @@ def main():
 
     main_model.load_weights(weights_file)
 
-    color_image = misc.imresize(misc.imread(sys.argv[1]), (im_width, im_height))
+    files = os.listdir(sys.argv[1])
 
-    if len(color_image.shape) > 2:
-        color_image = generate_adaptive_bw_image(color_image)
+    for file in files:
+        color_image = misc.imresize(
+            misc.imread(os.path.join(sys.argv[1], file)), (im_width, im_height))
 
-    color_image = color_image[None, ..., None] / 255
+        imsave("results/" + file + "_original.png", color_image)
 
-    imshow(np.uint8(main_model.predict(color_image)[0] * 255))
-    show()
+        if len(color_image.shape) > 2:
+            color_image = generate_adaptive_bw_image(color_image)
+
+        imsave("results/" + file + "_lines.png", color_image, cmap='gray')
+
+        color_image = color_image[None, ..., None] / 255
+
+        imsave("results/" + file + "_predicted.png",
+               np.uint8(main_model.predict(color_image)[0] * 255))
 
 
 if __name__ == "__main__":
